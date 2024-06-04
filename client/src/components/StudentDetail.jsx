7987fbe266abe2508e85fc0eb0b7b574e88e131c
Muhammad Uzair <rajputuzair58@gmail.com>
2024-06-04 11:48:07 +0500

    DarkMode

--- client/src/components/StudentDetail.jsx
+++ client/src/components/StudentDetail.jsx
@@ -25,44 +25,44 @@ const StudentDetail = () => {
 
   return (
     <div className='p-5'>
-      <div className={`p-5 min-w-[300px] bg-white max-w-full ease-linear duration-200 mx-auto rounded-lg shadow-lg`}>
+      <div className={`p-5 min-w-[300px] bg-white  dark:bg-slate-700 dark:text-white max-w-full ease-linear duration-200 mx-auto rounded-lg shadow-lg`}>
         <div className="flex gap-4 items-end">
         <button onClick={()=> navigate('/dashboard/manage_students')} className='active:bg-blue-100 rounded-full ease-linear duration-100 p-1'><ArrowLeftShort size={25}/></button>
         <h1 className="text-xl p-1 font-[600]">Student Information</h1>
         </div>
         <div className="flex justify-between mt-4">
           <div className='w-full p-2'>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Name</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.name}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Name</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.name}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>CNIC</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.cnic}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>CNIC</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.cnic}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Email</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.email}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Email</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.email}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Phone</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.phone}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Phone</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.phone}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Standard</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.standard}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Standard</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.standard}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Major</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.major}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Major</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.major}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Major</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{moment(studentInfo.created_at).format('ddd, DD-MMMM,YYYY')}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Major</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{moment(studentInfo.created_at).format('ddd, DD-MMMM,YYYY')}</th>
             </tr>
-            <tr className='flex gap-2'>
-              <th className='w-[30%] bg-white text-left text-gray-700 py-1 font-[600]'>Address</th>
-              <th className='w-[70%] bg-white text-left text-gray-700 py-1 font-[400]'>{studentInfo.address}</th>
+            <tr className='flex gap-2 rounded hover:bg-gray-200 px-3 dark:hover:bg-slate-800 ease-linear duration-200'>
+              <th className='w-[30%]  text-left text-gray-700 dark:text-white py-1 font-[600]'>Address</th>
+              <th className='w-[70%]  text-left text-gray-700 dark:text-white py-1 font-[400]'>{studentInfo.address}</th>
             </tr>
           </div>
           <div className='w-full'>
